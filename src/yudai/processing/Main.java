241e4d90698a2c6f8109560351dc69a1c62aaec1
岩崎雄大 <yudai2019@outlook.jp>
2021-10-30 11:41:25 +0900

    ぱくった

---
+++ src/yudai/processing/Main.java
@@ -0,0 +1,9 @@
+package yudai.processing;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+    public static void main(String[] args){
+        PApplet.main(Stage.class);
+    }
+}
